4d25414a5522a4b9166f8e19a86d14605b663ed4
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-05 16:13:39 +0300

    stores cart

--- js/task-7.js
+++ js/task-7.js
@@ -1,3 +1,6 @@
+////Не вирішено
+
+
 /*
  * Типів транзацкій всього два.
  * Можна покласти або зняти гроші з рахунку.
@@ -22,15 +25,27 @@ const account = {
    * Метод створює і повертає об'єкт транзакції.
    * Приймає суму і тип транзакції.
    */
-  createTransaction(amount, type) {},
+  createTransaction(amount, type) {
+    this.transactions = [
+      {amount,type,}
+    ]
 
+    console.log(this.transactions);
+  },
+
+  
   /*
    * Метод відповідає за додавання суми до балансу.
    * Приймає суму танзакції.
    * Викликає createTransaction для створення об'єкта транзакції
    * після чого додає його в історію транзакцій
    */
-  deposit(amount) {},
+  deposit(amount) {
+
+    this.balance += amount;
+    this.createTransaction(amount, type);
+
+  },
 
   /*
    * Метод відповідає за зняття суми з балансу.
@@ -41,17 +56,28 @@ const account = {
    * Якщо amount більше, ніж поточний баланс, виводь повідомлення
    * про те, що зняття такої суми не можливо, недостатньо коштів.
    */
-  withdraw(amount) {},
+  withdraw(amount) {
+    this.balance -= amount;
+    this.createTransaction(amount, type);
+    if (amount > this.balance) {
+      console.log('Недостатньо коштів на рахунку. ');
+    }
+  },
 
   /*
    * Метод повертає поточний баланс
    */
-  getBalance() {},
+  getBalance() {
+    // console.log(this.balance);
+    return this.balance;
+  },
 
   /*
    * Метод шукає і повертає об'єкт транзакції по id
    */
-  getTransactionDetails(id) {},
+  getTransactionDetails(id) {
+
+  },
 
   /*
    * Метод повертає кількість коштів
@@ -59,3 +85,4 @@ const account = {
    */
   getTransactionTotal(type) {},
 };
+
