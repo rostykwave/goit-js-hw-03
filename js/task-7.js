80fdf95b0fba1e57a6fa5e1970aaf4df44cb8c6a
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-07 21:24:57 +0300

    7 pending

--- js/task-7.js
+++ js/task-7.js
@@ -1,6 +1,3 @@
-////Не вирішено
-
-
 /*
  * Типів транзацкій всього два.
  * Можна покласти або зняти гроші з рахунку.
@@ -25,27 +22,15 @@ const account = {
    * Метод створює і повертає об'єкт транзакції.
    * Приймає суму і тип транзакції.
    */
-  createTransaction(amount, type) {
-    this.transactions = [
-      {amount,type,}
-    ]
+  createTransaction(amount, type) {},
 
-    console.log(this.transactions);
-  },
-
-  
   /*
    * Метод відповідає за додавання суми до балансу.
    * Приймає суму танзакції.
    * Викликає createTransaction для створення об'єкта транзакції
    * після чого додає його в історію транзакцій
    */
-  deposit(amount) {
-
-    this.balance += amount;
-    this.createTransaction(amount, type);
-
-  },
+  deposit(amount) {},
 
   /*
    * Метод відповідає за зняття суми з балансу.
@@ -56,28 +41,17 @@ const account = {
    * Якщо amount більше, ніж поточний баланс, виводь повідомлення
    * про те, що зняття такої суми не можливо, недостатньо коштів.
    */
-  withdraw(amount) {
-    this.balance -= amount;
-    this.createTransaction(amount, type);
-    if (amount > this.balance) {
-      console.log('Недостатньо коштів на рахунку. ');
-    }
-  },
+  withdraw(amount) {},
 
   /*
    * Метод повертає поточний баланс
    */
-  getBalance() {
-    // console.log(this.balance);
-    return this.balance;
-  },
+  getBalance() {},
 
   /*
    * Метод шукає і повертає об'єкт транзакції по id
    */
-  getTransactionDetails(id) {
-
-  },
+  getTransactionDetails(id) {},
 
   /*
    * Метод повертає кількість коштів
@@ -85,4 +59,108 @@ const account = {
    */
   getTransactionTotal(type) {},
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ////Не вирішено
+
+
+// /*
+//  * Типів транзацкій всього два.
+//  * Можна покласти або зняти гроші з рахунку.
+//  */
+// const Transaction = {
+//   DEPOSIT: 'deposit',
+//   WITHDRAW: 'withdraw',
+// };
+
+// /*
+//  * Кожна транзакція - це об'єкт з властивостями: id, type і amount
+//  */
+
+// const account = {
+//   // Поточний баланс рахунку
+//   balance: 0,
+
+//   // Історія транзакцій
+//   transactions: [],
+
+//   /*
+//    * Метод створює і повертає об'єкт транзакції.
+//    * Приймає суму і тип транзакції.
+//    */
+//   createTransaction(amount, type) {
+//     this.transactions = [
+//       {amount,type,}
+//     ]
+
+//     console.log(this.transactions);
+//   },
+
+  
+//   /*
+//    * Метод відповідає за додавання суми до балансу.
+//    * Приймає суму танзакції.
+//    * Викликає createTransaction для створення об'єкта транзакції
+//    * після чого додає його в історію транзакцій
+//    */
+//   deposit(amount) {
+
+//     this.balance += amount;
+//     this.createTransaction(amount, type);
+
+//   },
+
+//   /*
+//    * Метод відповідає за зняття суми з балансу.
+//    * Приймає суму танзакції.
+//    * Викликає createTransaction для створення об'єкта транзакції
+//    * після чого додає його в історію транзакцій.
+//    *
+//    * Якщо amount більше, ніж поточний баланс, виводь повідомлення
+//    * про те, що зняття такої суми не можливо, недостатньо коштів.
+//    */
+//   withdraw(amount) {
+//     this.balance -= amount;
+//     this.createTransaction(amount, type);
+//     if (amount > this.balance) {
+//       console.log('Недостатньо коштів на рахунку. ');
+//     }
+//   },
+
+//   /*
+//    * Метод повертає поточний баланс
+//    */
+//   getBalance() {
+//     // console.log(this.balance);
+//     return this.balance;
+//   },
+
+//   /*
+//    * Метод шукає і повертає об'єкт транзакції по id
+//    */
+//   getTransactionDetails(id) {
+
+//   },
+
+//   /*
+//    * Метод повертає кількість коштів
+//    * певного типу транзакції з усієї історії транзакцій
+//    */
+//   getTransactionTotal(type) {},
+// };
 
