3332cce5279445b2a55a74cfddc623366c0c2d78
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-07 18:20:54 +0300

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -4,28 +4,66 @@ const user = {
   hobby: 'html',
   premium: true,
 
-  changeName(newName) {
-    this.name = newName;
-  },
-
-  changeHobby(newHobby) {
-    this.hobby = newHobby;
-  },
-
-  changePremium(newPremium) {
-    this.premium = newPremium;
-  },
-
 };
+//до
 console.table(user);
 
-// user.name = 'happy';
-// user.hobby = 'skydiving';
-// user.premium = false;
 
-user.changeName('happy');
-user.changeHobby('skydiving');
-user.changePremium(false);
-//виводить вміст об'єкта user в форматі ключ:значення використовуючи Object.keys() і for...of
+user.mood = 'happy';
 
+user.hobby = 'skydiving';
+
+user.premium = false;
+
+const keys = Object.keys(user);
+    for (const key of keys) {
+      console.log(key,': ', user[key]);
+    }
+
+//після
 console.table(user);
+
+
+
+/////Інший спосіб, шляхом додавання вбудованих методів
+
+// const user = {
+//   name: 'Mango',
+//   age: 20,
+//   hobby: 'html',
+//   premium: true,
+
+//   addMood(newMood){
+//   this.mood = newMood;
+//   },
+  
+//   changeHobby(newHobby) {
+//     this.hobby = newHobby;
+//   },
+
+//   changePremium(newPremium) {
+//     this.premium = newPremium;
+//   },
+
+//   content() {
+//     const keys = Object.keys(this);
+//     for (const key of keys) {
+//       console.log(key,': ', this[key]);
+//     }
+    
+//   }
+
+// };
+
+// console.table(user);
+
+// user.mood('happy');
+
+// user.changeHobby('skydiving');
+
+// user.changePremium(false);
+
+// user.content();
+
+
+// console.table(user);
